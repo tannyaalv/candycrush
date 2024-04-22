fb44ee4023bed60266335a423be868a323cbe2b8
Tanya Alvarez-Salsedo <tanyaalvsal@tamu.edu>
2024-04-22 10:16:20 -0500

    Initial Commit

---
+++ candycrush/candycrushApp.swift
@@ -0,0 +1,17 @@
+//
+//  candycrushApp.swift
+//  candycrush
+//
+//  Created by Alvarez-Salsedo, Tanya on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct candycrushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
